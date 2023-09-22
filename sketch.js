706facb0d159c313f7e720a07c21b5a4d14ed3cd
Lucaseduardoab <133132198+Lucaseduardoab@users.noreply.github.com>
2023-09-22 10:16:07 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,11 +2,11 @@ function setup() {
   createCanvas(600, 600); 
   background("skyblue");
 }
-//console.log(mouseIsPressed)
 function draw() {
-  if (mouseIsPressed){ 
-  rect(mouseX, mouseY, 20, 10)
-} 
-  fill("black")
-  stroke("black")
+  //console.log(mouseIsPressed)
+  if (mouseIsPressed || touches.lenght > 0) { 
+    rect(mouseX, mouseY, 20, 10);
+  } 
+  fill("black");
+  stroke("black");
 }
